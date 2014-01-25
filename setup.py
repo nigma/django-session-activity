d56571f455996f42f178347555d4a53e8f8b039f
Filip Wasilewski <en@ig.ma>
2014-01-25 02:15:45 +0100

    Correct license classifier

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Django",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Natural Language :: English",
         "Programming Language :: Python :: 2",
